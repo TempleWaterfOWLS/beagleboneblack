c0f0a5bb9bf0202fce0063f72fafe68390bb17e8
Zack_Smith <zack.smith@temple.edu>
2014-11-03 16:23:05 -0500

    Added threading which kind of works.

--- beaglebone_webpage/webserver.py
+++ beaglebone_webpage/webserver.py
@@ -6,16 +6,15 @@ Created by Zack Smith 10/31/14
 # Imports for website generation
 from flask import *
 import os
-# Imports for motor integration
-from multiprocessing import Process
+# Imports for motor integration - use thread to spawn threads and adjust global motor vals
+from threading import Thread
 import time
 # import motor_control as mc
-
+M1 = -.1; M2 = -.1
 # Create instance of app
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-with open('input_args', 'w') as in_file:
-    in_file.write('0.0\n0.0')
+
 # Function to set up main page and motor logic
 @app.route('/', methods=['GET','POST'])
 def main_page():
@@ -29,11 +28,11 @@ def main_page():
         session['M1'] = 0.0
     if 'M2' not in session.keys():
         session['M2'] = 0.0
-
+    hold_val = [session['M1'], session['M2']]
     # Check for post response
     if request.method == 'POST':
         # Parse request keys for info
-        if 'M1' in request.form.keys() :
+        if 'M1' in request.form.keys() and not session['stop']:
             try:
                 # Force max/min vals
                 session['M1'] = float(request.form['M1'])
@@ -44,7 +43,7 @@ def main_page():
             except ValueError:
                 session['error'] = True
         # Check M2 Value
-        elif 'M2' in request.form.keys():
+        elif 'M2' in request.form.keys() and not session['stop']:
             try:
                 # Force max/min vals
                 session['M2'] = float(request.form['M2'])
@@ -58,13 +57,13 @@ def main_page():
         elif 'stop' in request.form.keys():
             session['stop'] = not session['stop']
             if session['stop'] == False:
-                with open('input_args', 'w') as arg_file:
-                    arg_file.write(str(session['M1'])+'\n')
-                    arg_file.write(str(session['M2']))
+                hold_val[0] = session['M1']
+                hold_val[1] = session['M2']
+                session['M1'] = 0.0
+                session['M2'] = 0.0
             else:
-                with open('input_args', 'w') as arg_file:
-                    arg_file.write(str(0.0) + '\n')
-                    arg_file.write(str(0.0))
+                session['M1'] = hold_val[0]
+                session['M2'] = hold_val[1]
 
         # Got unexpected POST
         else:
@@ -76,9 +75,42 @@ def main_page():
     print "VALUES IN SESSION DICT"
     for keys in session.keys():
         print 'Key: ' + keys + ' Value: ' + str(session[keys])
-        
+    print "Session vars:"
+    print session['M1'], session['M2']
+    set_globvar_M1(session['M1'])
+    set_globvar_M2(session['M2'])
     # Return Template
     return render_template('backup.html',session=session)
 
+def set_globvar_M1(t_val):
+    global M1
+    M1 = t_val
+
+def set_globvar_M2(t_val):
+    global M2
+    M2 = t_val
+'''
+def spawn_threads():
+    while 1:
+        print "Spawning thread..."
+        child_thread = Thread(target=print_thrust)
+        child_thread.daemon = True
+        child_thread.start()
+        time.sleep(5)
+'''
+def print_thrust():
+    while not isinstance(M1,int) and not isinstance(M2,int):
+        if (M1 == -.1 and M2 == -.1):
+            continue
+        print "Print Thrust"
+        print M1, M2
+        print type(M1), type(M2)
+        print "Ending thrust..."
+        time.sleep(1)
+        print "execution finish"
+
 if __name__ == '__main__':    
+    t = Thread(target=print_thrust)
+    t.daemon = True
+    t.start()
     app.run(debug=True)
